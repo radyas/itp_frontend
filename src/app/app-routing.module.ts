7fe64ff7e5e793cff52b49c6b17b88fda882492b
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2020-09-15 15:57:48 +0530

    base theme corrections

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+import { FullLayoutComponent } from "./layouts/full/full-layout.component";
+import { ContentLayoutComponent } from "./layouts/content/content-layout.component";
+
+import { Full_ROUTES } from "./shared/routes/full-layout.routes";
+import { CONTENT_ROUTES } from "./shared/routes/content-layout.routes";
+
+import { AuthGuard } from './shared/auth/auth-guard.service';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'full-layout',
+    pathMatch: 'full',
+  },
+  { path: '', component: FullLayoutComponent, data: { title: 'full Views' }, children: Full_ROUTES, canActivate: [AuthGuard] },
+  { path: '', component: ContentLayoutComponent, data: { title: 'content Views' }, children: CONTENT_ROUTES, canActivate: [AuthGuard] },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+
+}
--- src/app/shared/services/config.service.ts
+++ src/app/shared/services/config.service.ts
@@ -15,14 +15,14 @@ export class ConfigService {
     setConfigValue() {
         this.templateConf = {
             layout: {
-                variant: 'Light', // options:  Dark, Light & Transparent
+                variant: 'Transparent', // options:  Dark, Light & Transparent
                 dir: 'ltr', // Options: ltr, rtl
                 sidebar: {
                     collapsed: false, // options: true, false
                     size: 'sidebar-md', // Options: 'sidebar-lg', 'sidebar-md', 'sidebar-sm'
                   // tslint:disable-next-line:max-line-length
-                    backgroundColor: 'man-of-steel', // Options: 'black', 'pomegranate', 'king-yna', 'ibiza-sunset', 'flickr', 'purple-bliss', 'man-of-steel', 'purple-love'
-                    backgroundImage: true, // Options: true, false | Set true to show background image
+                    backgroundColor: 'pomegranate', // Options: 'black', 'pomegranate', 'king-yna', 'ibiza-sunset', 'flickr', 'purple-bliss', 'man-of-steel', 'purple-love'
+                    backgroundImage: false, // Options: true, false | Set true to show background image
                     backgroundImageURL: 'assets/img/sidebar-bg/01.jpg'
                 }
             }
